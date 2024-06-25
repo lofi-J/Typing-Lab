b5c0dcc3b94beffca7d12ca939dbaac5659ab0a7
lofi-J <jsj2505@gmail.com>
2024-06-25 17:27:53 +0900

    [수정] 헤더
     - 헤더 컨텐츠 static파일에서 관리

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,20 +1,42 @@
+'use client';
+
 import Link from "next/link";
-import styles from "@/styles/Header.module.css";
+import style from "@/styles/Header.module.css";
 import {Playfair_Display} from "next/font/google";
 import ToggleTheme from "@/component/ToggleTheme";
+import TextWriterAnimation from "@/component/TextWriterAnimation";
+import {useState} from "react";
+import {usePathname} from "next/navigation";
+import headers from "@/static/header";
 
 
 const font = Playfair_Display({ subsets: ["latin"]});
 
 const Header = () => {
+  const pathname = usePathname();
+  const [reWriteLogo, setReWriteLogo] = useState(false);
+  
+  const toggleRewriteLogo = () => {
+    setReWriteLogo(prev => !prev);
+  }
+  
+  const onClickLogo = () => {
+    if (pathname === '/') {
+      toggleRewriteLogo();
+    }
+  }
+  
   
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <Link href={'/'} className={`${font.className} ${styles.logo}`}>Lofi Typing</Link>
-        <div className={styles.link_wrap}>
-          <Link href={'/typing'}>Typing</Link>
-          <Link href={'/auth'}>Login</Link>
+    <header className={style.header}>
+      <nav className={style.nav}>
+        <Link href={'/'} className={`${font.className} ${style.logo}`} onClick={onClickLogo}>
+          <TextWriterAnimation text={'Lofi Typing'} delay={80} rewrite={reWriteLogo} playOnRender={false} />
+        </Link>
+        <div className={style.link_wrap}>
+          {headers.map((item, index) => (
+            <Link key={index} href={item.link}>{item.title}</Link>
+          ))}
           <ToggleTheme />
         </div>
       </nav>
---
+++ static/header.ts
@@ -0,0 +1,7 @@
+const headers = [
+  {title: 'Typing', link: '/typing'},
+  {title: 'Login', link: '/login'},
+  {title: 'contents', link: '/contents'}
+]
+
+export default headers;
