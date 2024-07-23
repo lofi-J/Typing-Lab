b6bc4578052cea5823277cd157efe5f2342c157d
lofi-J <jsj2505@naver.com>
2024-07-24 01:39:58 +0900

    [개발] Loading.tsx

--- component/Loading/Loading.tsx
+++ component/Loading/Loading.tsx
@@ -3,8 +3,9 @@ import styles from "./Loading.module.css";
 export const Loading = () => {
   return (
     <div className={styles.wrap}>
-      <div className={styles.ping1}/>
-      <div className={styles.ping2}/>
+      <div className={`${styles.ping} ${styles.ping1}`}/>
+      <div className={`${styles.ping} ${styles.ping2}`}/>
+      <div className={`${styles.ping} ${styles.ping3}`}/>
     </div>
   );
 }
