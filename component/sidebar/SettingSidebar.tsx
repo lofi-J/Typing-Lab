80b6f7f3cc3cd04f750c33b4fa31bdc2fa80615c
lofi-J <jsj2505@gmail.com>
2024-08-11 18:51:18 +0900

    [개발 / 디자인] settings 폰트

--- component/sidebar/SettingSidebar.tsx
+++ component/sidebar/SettingSidebar.tsx
@@ -16,8 +16,19 @@ interface ISettingSidebar {
   fontSize: number;
   fontWeight: TWeight;
 }
+
+const MAX_FONT_SIZE = 25;
+const MIN_FONT_SIZE = 12;
+const MAX_FONT_WEIGHT = 900;
+const MIN_FONT_WEIGHT = 100;
+
 const SettingSidebar = ({close, settings, setSettings, fontSize, fontWeight}: ISettingSidebar) => {
   const ref = useCloseOutside(close);
+  
+  const sampleCss: CSSProperties = {
+    fontSize: fontSize,
+    fontWeight: fontWeight
+  }
   
   const changeSettings = <K extends keyof ISettings>(key: K, value: ISettings[K]) => {
     setSettings({
@@ -26,9 +37,17 @@ const SettingSidebar = ({close, settings, setSettings, fontSize, fontWeight}: IS
     });
   }
   
-  const sampleCss: CSSProperties = {
-    fontSize: fontSize,
-    fontWeight: fontWeight
+  const onChangeFont = (type: 'size' | 'weight', dir: 'up' | 'down') => {
+    const isSize = type === 'size';
+    const delta = dir === 'up' ? 1 : -1;
+    const prevValue = isSize ? settings.fontSize : settings.fontWeight;
+    const nextValue = isSize ? prevValue + delta : prevValue + (delta * 100);
+    
+    const [min, max] = isSize ? [MIN_FONT_SIZE, MAX_FONT_SIZE] : [MIN_FONT_WEIGHT, MAX_FONT_WEIGHT];
+    
+    if (nextValue >= min && nextValue <= max) {
+      changeSettings(isSize ? 'fontSize' : 'fontWeight', nextValue as ISettings["fontWeight"]);
+    }
   }
   
   
@@ -47,12 +66,14 @@ const SettingSidebar = ({close, settings, setSettings, fontSize, fontWeight}: IS
             <div className={styles.btn_wrap}>
               <button
                 className={settings.appearance === 'basic' ? styles.active : ''}
+                disabled={settings.appearance === 'basic'}
                 onClick={() => changeSettings('appearance', 'basic')}
               >
                 Basic Style
               </button>
               <button
                 className={settings.appearance === 'zen' ? styles.active : ''}
+                disabled={settings.appearance === 'zen'}
                 onClick={() => changeSettings('appearance', 'zen')}
               >
                 Zen Mode
@@ -75,18 +96,37 @@ const SettingSidebar = ({close, settings, setSettings, fontSize, fontWeight}: IS
             <div className={styles.font}>
               <h3>Font Size</h3>
               <div className={styles.btn_wrap}>
-                // TODO 폰트 크기, 굵기 변경 제한 함수를 전달해야함
-                <button onClick={() => changeSettings('fontSize', settings.fontSize + 1)}>
+                <button
+                  className={settings.fontSize >= MAX_FONT_SIZE ? styles.disable_btn : ''}
+                  disabled={settings.fontSize >= MAX_FONT_SIZE}
+                  onClick={() => onChangeFont('size', 'up')}
+                >
                   <BsPlus size={20} />
                 </button>
-                <button onClick={() => changeSettings('fontSize', settings.fontSize - 1)}>
+                <button
+                  className={settings.fontSize <= MIN_FONT_SIZE ? styles.disable_btn : ''}
+                  disabled={settings.fontSize <= MIN_FONT_SIZE}
+                  onClick={() => onChangeFont('size', 'down')}
+                >
                   <HiOutlineMinus size={20} />
                 </button>
               </div>
               <h3>Font Weight</h3>
               <div className={styles.btn_wrap}>
-                <button><BsPlus size={20} /></button>
-                <button><HiOutlineMinus size={20} /></button>
+                <button
+                  className={settings.fontWeight >= MAX_FONT_WEIGHT ? styles.disable_btn : ''}
+                  disabled={settings.fontWeight >= MAX_FONT_WEIGHT}
+                  onClick={() => onChangeFont('weight', 'up')}
+                >
+                  <BsPlus size={20} />
+                </button>
+                <button
+                  className={settings.fontWeight <= MIN_FONT_WEIGHT ? styles.disable_btn : ''}
+                  disabled={settings.fontWeight <= MIN_FONT_WEIGHT}
+                  onClick={() => onChangeFont('weight', 'down')}
+                >
+                  <HiOutlineMinus size={20}/>
+                </button>
               </div>
               <div className={styles.font_preview}>
                 <span style={sampleCss}>한글 / English</span>
